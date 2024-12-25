6081e29de47427162cacbe5e0e77d25c787c642a
zlatonn <tonkar2541@hotmail.com>
2024-12-25 15:24:05 +0700

    ✅ Apply usePages to SideBar component

--- src/components/side-bar/SideBar.jsx
+++ src/components/side-bar/SideBar.jsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react";
+import usePages from "../../hooks/usePages";
 import Step from "./Step";
+import data from "../../data";
 
 function SideBar() {
+  const currentPageNo = usePages((state) => state.pageNo);
+  const setPage = usePages((state) => state.setPage);
+
+  const [pageData, setPageData] = useState({});
+
+  useEffect(() => {
+    const currentPageData = data.find((page) => page.pageNo === currentPageNo);
+    setPageData(currentPageData || {});
+  }, [currentPageNo]);
+
   return (
     <div className="w-[30%] h-full pt-5 pb-20 px-5 bg-[#304767] text-white flex flex-col gap-10">
       <p className="text-xl">💰 react money tracker</p>
       <div className="flex flex-col gap-3">
-        <p className="text-xl">Step 1</p>
-        <p className="text-sm opacity-80">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, a!</p>
+        <p className="text-xl">Step {pageData.pageNo + 1}</p>
+        <p className="text-sm opacity-80">{pageData.suggestText}</p>
       </div>
       <div className="flex flex-col gap-16">
-        <Step />
-        <Step />
-        <Step />
+        {data.map((e) => (
+          <Step
+            key={e.pageNo}
+            pageNo={e.pageNo}
+            name={e.pageName}
+            line={e.line}
+            state={e.pageNo === currentPageNo ? "active" : e.pageNo < currentPageNo ? "complete" : null}
+            handle={() => setPage(e.pageNo)}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/side-bar/Step.jsx
+++ src/components/side-bar/Step.jsx
@@ -1,9 +1,26 @@
-function Step() {
+// eslint-disable-next-line react/prop-types
+function Step({ pageNo, name, line, state, handle }) {
   return (
-    <div className="relative flex items-center gap-3 cursor-pointer ">
-      <div className="w-7 h-7 border-2 border-white rounded-full flex justify-center items-center text-sm text-white">1</div>
-      <span>some menu</span>
-      <div className="absolute left-[13px] top-7 h-16 border-l-2 border-white"></div>
+    <div className="relative flex items-center gap-3 cursor-pointer " onClick={handle}>
+      <div
+        className={`w-7 h-7 border-2 rounded-full flex justify-center items-center text-sm ${
+          state === "active"
+            ? `bg-[#304767] text-white border-white`
+            : state === "complete"
+            ? `bg-white text-[#304767] border-white`
+            : `bg-[#304767] text-white border-white opacity-10`
+        } `}
+      >
+        {pageNo + 1}
+      </div>
+      <span className={`${state ? `opacity-none` : `opacity-10`}`}>{name}</span>
+      {line ? (
+        <div
+          className={`absolute left-[13px] top-7 h-16 border-l-2 border-white cursor-default ${
+            state === "complete" ? `opacity-none` : `opacity-10`
+          }`}
+        ></div>
+      ) : null}
     </div>
   );
 }
