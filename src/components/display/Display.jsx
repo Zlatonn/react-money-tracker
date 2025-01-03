e32e3f3d8067e4c9be6221a25f8fa79d16af4e21
zlatonn <tonkar2541@hotmail.com>
2025-01-03 16:04:54 +0700

    ✅ Apply useFinance to display component

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,15 +1,77 @@
+import useFinance from "../../hooks/useFinance";
+
 function Display() {
+  const income = useFinance((state) => state.income);
+  const expense = useFinance((state) => state.expense);
+
+  // Calculation monthly
+  const sumIncomMonthly = Object.values(income)
+    .filter((item) => item.per === "month")
+    .map((item) => item.value || 0)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  const sumExpenseMonthly = Object.values(expense)
+    .filter((item) => item.per === "month")
+    .map((item) => item.value || 0)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  const cashFlowMonthly = sumIncomMonthly - sumExpenseMonthly;
+
+  // Calculation Annual
+  const sumIncomAnnual =
+    Object.values(income)
+      .filter((item) => item.per === "year")
+      .map((item) => item.value || 0)
+      .reduce((acc, curr) => acc + curr, 0) +
+    sumIncomMonthly * 12;
+
+  const sumExpenseAnnual =
+    Object.values(expense)
+      .filter((item) => item.per === "year")
+      .map((item) => item.value || 0)
+      .reduce((acc, curr) => acc + curr, 0) +
+    sumExpenseMonthly * 12;
+
+  const cashFlowAnnual = sumIncomAnnual - sumExpenseAnnual;
+
+  // function return sign number
+  const signStatus = (number) => {
+    return number >= 0 ? true : false;
+  };
+
   return (
     <div className="w-[15%] py-20 px-5 flex flex-col gap-5 text-sm">
       <div>
-        <p className="underline">Mothly</p>
-        <p>Income:</p>
-        <p>Expenese:</p>
+        <p className="underline font-bold">Mothly</p>
+        <p>
+          Income: <span className="text-green-500">{sumIncomMonthly.toLocaleString()}</span>
+        </p>
+        <p>
+          Expenese: <span className="text-red-500">{sumExpenseMonthly.toLocaleString()}</span>
+        </p>
+        <p>
+          Cashflow:
+          <span className={`${signStatus(cashFlowMonthly) ? "text-green-500" : "text-red-500"}`}>
+            {signStatus(cashFlowMonthly) && " + "}
+            {cashFlowMonthly.toLocaleString()}
+          </span>
+        </p>
       </div>
       <div>
-        <p className="underline">Annual</p>
-        <p>Income:</p>
-        <p>Expenese:</p>
+        <p className="underline font-bold">Annual</p>
+        <p>
+          Income: <span className="text-green-500">{sumIncomAnnual.toLocaleString()}</span>
+        </p>
+        <p>
+          Expenese: <span className="text-red-500">{sumExpenseAnnual.toLocaleString()}</span>
+        </p>
+        <p>
+          Cashflow:
+          <span className={`${signStatus(cashFlowAnnual) ? "text-green-500" : "text-red-500"}`}>
+            {signStatus(cashFlowAnnual) && " + "}
+            {cashFlowAnnual.toLocaleString()}
+          </span>
+        </p>
       </div>
     </div>
   );
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,13 +1,18 @@
 // eslint-disable-next-line react/prop-types
 function Input({ type, value = 0, onChange }) {
-  // call onChange function from parent
   const handleInput = (e) => {
-    onChange(e.target.value);
+    onChange(e.target.value === "" ? 0 : parseFloat(e.target.value));
   };
 
   return (
     <>
-      <input type={type} className="border-[1px] rounded-md px-3 py-2" value={value} onChange={handleInput} min={0} />
+      <input
+        type={type}
+        className="outline-none border-[1px] rounded-md px-3 py-2 focus:bg-gray-100 "
+        value={value}
+        onChange={handleInput}
+        min={0}
+      />
     </>
   );
 }
